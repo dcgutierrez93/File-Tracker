ad89e4cbe8b846e238eac92b8a82a9a1c676dae1
dcgutierrez93 <dcgutierrez93@yahoo.com>
2018-10-11 13:33:43 -0500

    First Commit

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// Dependencies
+// =====================
+const express = require('express');
+
+// Setup our express app
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Static directory
+app.use(express.static(__dirname + '/public'));
+
+// Require Routes
+require("./routes/htmlRoutes.js")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
