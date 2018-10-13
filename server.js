74dcb7296e935ee2a801e6a660be656ba97212ea
David Gutierrez <dcgutierrez93@yahoo.com>
2018-10-13 10:58:09 -0500

    added api routes for file upload

--- server.js
+++ server.js
@@ -3,17 +3,45 @@
 // Dependencies
 // =====================
 const express = require('express');
+const bodyParser = require('body-parser');
+const multer = require('multer');
 
 // Setup our express app
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+// bodyParser
+app.use(bodyParser.json());
 
 // Static directory
 app.use(express.static(__dirname + '/public'));
 
 // Require Routes
 require("./routes/htmlRoutes.js")(app);
+// require("./routes/apiRoutes.js")(app);
 
+// Handle Files storage
+var Storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, "./Images");
+    },
+    filename: function (req, file, callback) {
+        callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+    }
+});
+
+app.post("/api/Upload", function(req, res) {
+    upload(req, res, function(err) {
+        if (err) {
+            return res.end("Something went wrong!");
+        }
+        return res.end("File uploaded sucessfully!.");
+    });
+});
+
+var upload = multer({
+    storage: Storage
+}).array("imgUploader", 3);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
